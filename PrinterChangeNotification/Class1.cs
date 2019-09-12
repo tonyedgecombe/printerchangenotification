7da9c5118d841a70b3139741e67580e667654d5b
Tony Edgecombe <tonyedgecombe@gmail.com>
2019-09-12 11:25:56 +0100

    Initial native method declarations

--- PrinterChangeNotification/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PrinterChangeNotification
-{
-    public class Class1
-    {
-    }
-}
---
+++ PrinterChangeNotification/NativeMethods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PrinterChangeNotification
+{
+    internal class NativeMethods
+    {
+        //HANDLE WINAPI FindFirstPrinterChangeNotification(
+        //    __in HANDLE hPrinter,
+        //    DWORD fdwFilter,
+        //    DWORD fdwOptions,
+        //    __in_opt PVOID  pPrinterNotifyOptions
+        //);
+
+        [DllImport("winspool.drv", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindFirstPrinterChangeNotification(IntPtr hPrinter, UInt32 fdwFilter, UInt32 fdwOptions, IntPtr pPrinterNotifyOptions);
+
+        //BOOL WINAPI FindNextPrinterChangeNotification(
+        //    __in HANDLE hChange,
+        //    __out_opt PDWORD pdwChange,
+        //    __in_opt LPVOID pvReserved,
+        //    __out_opt LPVOID *ppPrinterNotifyInfo
+        //);
+
+        [DllImport("winspool.drv", SetLastError = true, CharSet = CharSet.Unicode)]
+
+        public static extern bool FindNextPrinterChangeNotification(IntPtr hChange, ref UInt32 pdwChange, IntPtr pvReserved, ref IntPtr ppPrinterNotifyInfo);
+
+        //BOOL WINAPI FreePrinterNotifyInfo(
+        //    __in PPRINTER_NOTIFY_INFO pPrinterNotifyInfo
+        //);
+
+        [DllImport("winspool.drv", SetLastError = true, CharSet = CharSet.Unicode)]
+
+        public static extern bool FreePrinterNotifyInfo(IntPtr pPrinterNotifyInfo);
+
+        //BOOL WINAPI FindClosePrinterChangeNotification(
+        //    __in HANDLE hChange
+        //);
+
+        [DllImport("winspool.drv", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool FindClosePrinterChangeNotification(IntPtr hChange);
+    }
+}
