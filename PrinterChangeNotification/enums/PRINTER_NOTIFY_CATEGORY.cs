c9176ba3e0251f14cf0a4394cb91a72d4dfeeafa
Tony Edgecombe <tonyedgecombe@gmail.com>
2019-09-20 10:38:25 +0100

    Added 2D to PRINTER_NOTIFY_CATEGORY

--- PrinterChangeNotification/enums/PRINTER_NOTIFY_CATEGORY.cs
+++ PrinterChangeNotification/enums/PRINTER_NOTIFY_CATEGORY.cs
@@ -4,11 +4,14 @@
 namespace PrinterChangeNotification.enums
 {
     // Note: These flags are different from the documentation
-    // TODO Verify which is correct
+    // A pull request has been sent to MS 
+    // https://github.com/MicrosoftDocs/win32/pull/45
 
+    
     [Flags]
     public enum PRINTER_NOTIFY_CATEGORY : UInt32
     {
+        PRINTER_NOTIFY_CATEGORY_2D  = 0x000000, // This is not defined in winspool.h but pulled from the documentation
         PRINTER_NOTIFY_CATEGORY_ALL = 0x001000,
         PRINTER_NOTIFY_CATEGORY_3D  = 0x002000,
     }
