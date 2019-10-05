cd8ae40c61519f17330ecd5b2f470cdfcafed5f6
Tony Edgecombe <tonyedgecombe@gmail.com>
2019-10-05 12:09:11 +0100

    Revert "Switched to C#8 style using"
    This reverts commit 4c90137203cc0594ee9c324c10fb5d77bcd8f2fe.

--- Monitor/Program.cs
+++ Monitor/Program.cs
@@ -14,20 +14,20 @@ namespace Monitor
 
         static void Main(string[] args)
         {
-            NotifyOptions notifyOptions = null;
+            NotifyOptions printerNotifyOptions = null;
 
             Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
             {
                 if (o.JobNotifyFields.Any() || o.JobNotifyFields.Any())
                 {
-                    notifyOptions = new NotifyOptions
+                    printerNotifyOptions = new NotifyOptions
                     {
                         Types = new List<NotifyOptionsType>()
                     };
 
                     if (o.JobNotifyFields.Any())
                     {
-                        notifyOptions.Types.Add(new NotifyOptionsType
+                        printerNotifyOptions.Types.Add(new NotifyOptionsType
                         {
                             Type = NOTIFY_TYPE.JOB_NOTIFY_TYPE,
                             Fields = o.JobNotifyFields.Cast<UInt32>().ToList(),
@@ -36,51 +36,52 @@ namespace Monitor
 
                     if (o.PrinterNotifyFields.Any())
                     {
-                        notifyOptions.Types.Add(new NotifyOptionsType
+                        printerNotifyOptions.Types.Add(new NotifyOptionsType
                         {
                             Type = NOTIFY_TYPE.PRINTER_NOTIFY_TYPE,
                             Fields = o.PrinterNotifyFields.Cast<UInt32>().ToList(),
                         });
                     }
                 }
-                MonitorPrinter(o.PrinterName, notifyOptions, o.PrinterChange);
+                MonitorPrinter(o.PrinterName, printerNotifyOptions, o.PrinterChange);
             });
         }
 
-        private static void MonitorPrinter(string printerName, NotifyOptions notifyOptions, PRINTER_CHANGE change)
+        private static void MonitorPrinter(string printerName, NotifyOptions printerNotifyOptions, PRINTER_CHANGE change)
         {
-            using var changeNotification = new ChangeNotification(printerName,
-                                            change,
-                                            PRINTER_NOTIFY_CATEGORY.PRINTER_NOTIFY_CATEGORY_ALL,
-                                            notifyOptions);
-
-            while (true)
+            using (var printerChangeNotification = new ChangeNotification(printerName,
+                    change,
+                    PRINTER_NOTIFY_CATEGORY.PRINTER_NOTIFY_CATEGORY_ALL,
+                    printerNotifyOptions))
             {
-                changeNotification.WaitOne();
-                NotifyInfo notifyInfo;
-                bool refresh = false;
-
-                do
+                while (true)
                 {
-                    notifyInfo = changeNotification.FindNextPrinterChangeNotification(refresh);
-                    WriteToConsole(notifyInfo);
+                    printerChangeNotification.WaitOne();
+                    NotifyInfo printerNotifyInfo;
+                    bool refresh = false;
 
-                    refresh = true; // For next iteration if data overflowed
-                } while ((notifyInfo.Flags & PRINTER_NOTIFY_INFO_DISCARDED) != 0);
+                    do
+                    {
+                        printerNotifyInfo = printerChangeNotification.FindNextPrinterChangeNotification(refresh);
+                        WriteToConsole(printerNotifyInfo);
+
+                        refresh = true; // For next iteration if data overflowed
+                    } while ((printerNotifyInfo.Flags & PRINTER_NOTIFY_INFO_DISCARDED) != 0);
+                }
             }
         }
 
-        private static void WriteToConsole(NotifyInfo notifyInfo)
+        private static void WriteToConsole(NotifyInfo printerNotifyInfo)
         {
-            Console.WriteLine($"Change: {notifyInfo.Change}");
+            Console.WriteLine($"Change: {printerNotifyInfo.Change}");
 
-            var printerNotifyData = notifyInfo.Data.Where(data => data.Type == (int) NOTIFY_TYPE.PRINTER_NOTIFY_TYPE);
+            var printerNotifyData = printerNotifyInfo.Data.Where(data => data.Type == (int) NOTIFY_TYPE.PRINTER_NOTIFY_TYPE);
             foreach (var printerNotifyInfoData in printerNotifyData)
             {
                 Console.WriteLine($"{(PRINTER_NOTIFY_FIELD) printerNotifyInfoData.Field} = {printerNotifyInfoData.Value}");
             }
 
-            var jobNotifyData = notifyInfo.Data.Where(data => data.Type == (int) NOTIFY_TYPE.JOB_NOTIFY_TYPE);
+            var jobNotifyData = printerNotifyInfo.Data.Where(data => data.Type == (int) NOTIFY_TYPE.JOB_NOTIFY_TYPE);
             foreach (var pair in jobNotifyData)
             {
                 Console.WriteLine($"{pair.Id}:{(JOB_NOTIFY_FIELD) pair.Field} = {pair.Value}");
